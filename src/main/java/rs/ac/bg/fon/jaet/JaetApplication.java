5e0489e68f86018b6e1969085bf543ae9ea5deef
Milan <milan_susa@hotmail.com>
2020-03-30 19:12:57 +0200

    Initial commit

---
+++ src/main/java/rs/ac/bg/fon/jaet/JaetApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.jaet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JaetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/rs/ac/bg/fon/jaet/JaetApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.jaet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
