b40bc6aee89c204a791fe62d6e7d511c59bfcb34
Milan <milan_susa@hotmail.com>
2020-03-30 20:23:58 +0200

    Add user model

---
+++ src/main/java/rs/ac/bg/fon/jaet/model/User.java
@@ -0,0 +1,23 @@
+package rs.ac.bg.fon.jaet.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+}
