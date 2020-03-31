bbc74eeb8cf85d52f6265600cc45105ea82f2330
Milan <milan_susa@hotmail.com>
2020-03-31 18:55:25 +0200

    Add restrictions to person

--- src/main/java/rs/ac/bg/fon/jaet/model/Person.java
+++ src/main/java/rs/ac/bg/fon/jaet/model/Person.java
@@ -1,12 +1,10 @@
 package rs.ac.bg.fon.jaet.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,7 +18,9 @@ public class Person {
     private Long id;
     private String firstName;
     private String lastName;
+    @Column(unique = true)
     private String email;
+    @JsonIgnore
     private String password;
     @OneToMany(mappedBy = "person")
     private List<Inference> inferences = new ArrayList<>();
